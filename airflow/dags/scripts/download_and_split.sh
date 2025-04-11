3e8b0d341b5355589d5ed41bd0be52a261944321
Roger Torres <torresroger776@gmail.com>
2025-04-11 11:44:21 -0400

    cleanup airflow files

--- airflow/dags/scripts/download_and_split.sh
+++
@@ -1,4 +0,0 @@
-yt-dlp --concurrent-fragments 5 --format m4a/bestaudio/best --output '%(id)s.%(ext)s' https://www.youtube.com/watch?v=${VIDEO_ID}
-mkdir -p ${VIDEO_ID}
-ffmpeg -i ${VIDEO_ID}.m4a -f segment -segment_time 900 -c copy -reset_timestamps 1 ${VIDEO_ID}/${VIDEO_ID}_%03d.m4a
-rm ${VIDEO_ID}.m4a
